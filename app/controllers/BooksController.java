a9006cc23c7dded1d439274772460c7fb41ffc45
robuntu <robuntu@robuntu.ca>
2018-04-17 09:42:53 -0400

    Tutorial 9-12

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    public Result index(){
+        return TODO;
+    }
+
+    //to create book
+    public Result create(){
+        return TODO;
+    }
+
+    //to save book
+    public Result save(){
+        return TODO;
+    }
+
+    //edit single book
+    public Result edit(Integer id){
+        return TODO;
+    }
+
+    public Result update(){
+        return TODO;
+    }
+
+    public Result destroy(Integer id) {
+        return TODO;
+    }
+
+    //for book details
+    public Result show(Integer id) {
+        return TODO;
+    }
+
+}
